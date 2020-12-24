2674a62f8d04a0e04a41debf3d1c0e3753261a19
AlexD2019 <alexander0derevianko@gmail.com>
2020-12-24 13:14:08 +0000

    Fourth task done

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -10,12 +10,71 @@ public class Cinema {
 		System.out.println("Enter the number of seats in each row: ");
 		int columns = scanner.nextInt();
 
-		if (rows * columns < 60) {
-			System.out.println("Total income: \n$" + rows * columns * 10);
-		} else {
-			int firstHalf = rows / 2;
-			System.out.println("Total income: \n$"
-					+ (firstHalf * columns * 10 + (rows - firstHalf) * columns * 8));
+		String[][] cinemaSeats = createSeats(rows, columns);
+
+		while (true) {
+			System.out.println("1. Show the seats\n2. Buy a ticket\n0. Exit");
+			String input = scanner.next();
+
+			switch (input) {
+				case "1":
+					showSeats(cinemaSeats);
+					break;
+				case "2":
+					buyATicket(cinemaSeats);
+					break;
+				case "0":
+					return;
+			}
 		}
+	}
+
+	private static void buyATicket(String[][] cinemaSeats) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter a row number:");
+		int rowOfUserSeat = scanner.nextInt();
+		System.out.println("Enter a seat number in that row:");
+		int columnOfUserSeat = scanner.nextInt();
+
+		if (cinemaSeats.length * cinemaSeats[0].length < 60) {
+			System.out.println("Ticket price: $10");
+		} else {
+			int firstHalf = (cinemaSeats.length - 1) / 2;
+			if (rowOfUserSeat <= firstHalf) {
+				System.out.println("Ticket price: $10");
+			} else {
+				System.out.println("Ticket price: $8");
+			}
+		}
+
+		cinemaSeats[rowOfUserSeat][columnOfUserSeat] = "B";
+	}
+
+	private static void showSeats(String[][] seats) {
+		System.out.println("Cinema:");
+
+		for (String[] ss : seats) {
+			for (String s : ss) {
+				System.out.print(s + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	private static String[][] createSeats(int rows, int columns) {
+		String[][] cinemaSeats = new String[rows + 1][columns + 1];
+		cinemaSeats[0][0] = "  ";
+		for (int i = 1; i <= columns; i++) {
+			cinemaSeats[0][i] = String.valueOf(i);
+		}
+
+		for (int i = 1; i < cinemaSeats.length; i++) {
+			cinemaSeats[i][0] = String.valueOf(i);
+			for (int j = 1; j < cinemaSeats[i].length; j++) {
+				cinemaSeats[i][j] = "S";
+			}
+		}
+
+		return cinemaSeats;
 	}
 }
